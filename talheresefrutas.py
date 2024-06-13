f0c8ceeea9d7b8ba288de03cd46fecd76e5b0e77
monica2024ti <166753990+monica2024ti@users.noreply.github.com>
2024-06-13 15:35:06 -0300

    Add files via upload

---
+++ talheresefrutas.py
@@ -0,0 +1,14 @@
+talheres=['faca','morango','concha','laranja','colher de chá']
+frutas=['uva','kiwi','banana','garfo','pera','colher']
+
+talheres.remove('morango')
+talheres.remove('laranja')
+talheres.append('garfo')
+talheres.append('colher')
+
+frutas.remove('garfo')
+frutas.remove('colher')
+frutas.append('morango')
+frutas.append('laranja')
+print('lista de compras atualizada :', talheres)
+print('lista de compras atualizada :', frutas)
